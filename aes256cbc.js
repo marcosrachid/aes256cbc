5ac51665d7a3a134b46f17e47f4f539c095b8356
xied5531 <xied5531@users.noreply.github.com>
2016-08-12 10:47:54 +0800

    Create aes256cbc.js

---
+++ aes256cbc.js
@@ -0,0 +1,27 @@
+(function (exports) {
+  'use strict'
+
+  var crypto = require('crypto')
+  var algorithm = 'aes-256-cbc'
+  var key = '1234567890-=!@#$%^&*()_+'
+  var _iv = 'IaMaHaPpYiVIvIV.'
+
+  function encrypt (plaintext) {
+    var sha256sum = crypto.createHash('sha256')
+    var _key = sha256sum.update(key).digest()
+    var cipher = crypto.createCipheriv(algorithm, _key, _iv)
+    var encrypted = cipher.update(plaintext, 'binary', 'hex') + cipher.final('hex')
+    return encrypted
+  }
+
+  function decrypt (encrypted) {
+    var sha256sum = crypto.createHash('sha256')
+    var _key = sha256sum.update(key).digest()
+    var decipher = crypto.createDecipheriv(algorithm, _key, _iv)
+    var decrypted = decipher.update(encrypted, 'hex', 'binary') + decipher.final('binary')
+    return decrypted
+  }
+
+  exports.encrypt = encrypt
+  exports.decrypt = decrypt
+})(module.exports)
